ff30aa146277b406193113decd3c2b6fe0f23f3c
Bumbus <sf@arillo.net>
2017-01-04 19:08:13 +0100

    parse multiple element relations from config

--- code/extensions/ElementsExtension.php
+++ code/extensions/ElementsExtension.php
@@ -1,110 +1,178 @@
 <?php
+class ElementsExtension extends DataExtension
+{
+    protected $_elementBaseClass;
 
-class ElementsExtension extends DataExtension {
+    public function __construct($elementBaseClass = 'ElementBase')
+    {
+        parent::__construct();
+        $this->_elementBaseClass = $elementBaseClass;
+    }
 
+   /* public static function parse_classes($relations)
+    {
+        if (!isset($relations))
+        {
+            user_error('you have to define the list of elements in your .yml config file');
+        }
 
-	protected $elementClass;
+        $baseClass = 'ElementBase';
+        $classes = [];
 
-	public function __construct($elementClass = 'ElementBase') {
-		parent::__construct();
-		$this->elementClass = $elementClass;
-	}
+        foreach ($relations as $key => $value)
+        {
+            if (ClassInfo::exists($value) && (is_a(singleton($value), $baseClass)))
+            {
+                if ($label = singleton($value)->stat('singular_name'))
+                {
+                    $classes[$value] = $label;
+                } else {
+                    $classes[$value] = $value;
+                }
+            }
+        }
+        return $classes;
+    }*/
 
-	// define has_many based on elementClass
-	public function extraStatics($class = null, $extension = null) {
-		return array('has_many' => array('Elements' => $this->elementClass));
-	}
+    // define has_many based on elementClass
+    public function extraStatics($class = null, $extension = null)
+    {
+        return array(
+            'has_many' => array('Elements' => $this->_elementBaseClass)
+        );
+    }
 
-	// private static $has_many = array(
-	// 	'Elements' => 'Element'
-	// 	);
-	// public function Elements(){
-	// 	$class = $this->elementClass;
-	// 	return $class::get()->filter(array('PageID' => $this->owner->ID))->sort(array('Sort'=>'ASC'));
-	// }
+    public function updateCMSFields(FieldList $fields)
+    {
+        if (!$this->owner->exists()) return;
 
-	public static function parseClasses($relations) {
-		if (!isset($relations)) {
-			user_error('you have to define the list of elements in your .yml config file');
-		}
-		$baseClass ='ElementBase';
-		$classes = array();
-		foreach ($relations as $key => $value) {
-			if (ClassInfo::exists($value) && (is_a(singleton($value), $baseClass))) {
-				if($label = singleton($value)->stat('singular_name')){
-					$classes[$value] = $label;
-				} else {
-					$classes[$value] = $value;
-				}
-			}
-		}
-		return $classes;
-	}
+        if ($realtionNames = $this->_elementRealtionNames())
+        {
+            foreach ($realtionNames as $key => $relationName)
+            {
+                $this->createGridFieldForRelation($fields, $relationName);
+            }
+        }
+        // die;
+    }
 
-	public function updateCMSFields(FieldList $fields) {
-		if (!$this->owner->exists()) return;
+    public function getCMSElements()
+    {
+        return $this->owner
+            ->Elements()
+            ->sort(array('Sort' => 'ASC'))
+        ;
+    }
 
-		// if (in_array($this->owner->ClassName, Config::inst()->get('OrganismExtension', 'excludeClasses'))) {
-		// 	$fields->addFieldToTab(
-		// 		'Root.Main',
-		// 		NoticeField::create(
-		// 			'NoOrganismsNotice',
-		// 			_t(
-		// 				'Page.NoOrganismsMessage',
-		// 				'This page generates content automatically and therefore does not allow creation of organisms'
-		// 				)
-		// 			)
-		// 		);
-		// 	return;
-		// }
+    public function onAfterPublish()
+    {
+        foreach($this->owner->Elements() as $element)
+        {
+            $element->write();
+            $element->publish('Stage', 'Live');
+        }
+    }
 
-		if ($relations = ElementsExtension::parseClasses($this->owner->stat('elements'))) {
+    public function createGridFieldForRelation(FieldList $fields, $relationName)
+    {
+        if ($elementClasses = $this->_elementClassesForRealtion($relationName))
+        {
+            // sort relations
+            asort($elementClasses);
 
-			$config = GridFieldConfig_RelationEditor::create()
-			->removeComponentsByType('GridFieldDeleteAction')
-			->removeComponentsByType('GridFieldAddNewButton')
-			->removeComponentsByType('GridFieldAddExistingAutocompleter')
-			->addComponents(
-				new GridFieldOrderableRows('Sort'),
-				$multiClass = new GridFieldAddNewMultiClass()
-				);
+            $config = GridFieldConfig_RelationEditor::create()
+                ->removeComponentsByType('GridFieldDeleteAction')
+                ->removeComponentsByType('GridFieldAddExistingAutocompleter')
+            ;
 
-      		// sort relations
-			asort($relations);
-			$multiClass->setClasses($relations);
+            if (ClassInfo::exists('GridFieldOrderableRows'))
+            {
+                $config->addComponent(new GridFieldOrderableRows('Sort'));
+            }
 
-			$paginator = $config->getComponentByType('GridFieldPaginator');
-			$paginator->setItemsPerPage(50);
+            if (count($elementClasses) > 1)
+            {
+                $config
+                    ->removeComponentsByType('GridFieldAddNewButton')
+                    ->addComponent($multiClass = new GridFieldAddNewMultiClass())
+                ;
 
-			$dataColumns = $config->getComponentByType('GridFieldDataColumns');
+                // $multiClass->setClasses($elementClasses);
+                $multiClass->setClasses($this->_elementClassesForDropdown($elementClasses));
+            }
 
-			$dataColumns->setDisplayFields(array(
-				// 'Icon' => 'Icon',
-				'singular_name'=> 'Type',
-				'Title' => 'Title',
-				'Languages' => 'Lang'
-				));
+            $config
+                ->getComponentByType('GridFieldPaginator')
+                ->setItemsPerPage(50)
+            ;
 
-			$fields->addFieldToTab('Root.Elements',
-				$gridfieldItems = GridField::create(
-					'Elements',
-					_t('ElementsExtension.Elements', 'Elements'),
-					$this->owner->getCMSElements(),
-					$config
-					)
-				);
+            $columns = [
+                // 'Icon' => 'Icon',
+                'singular_name'=> 'Type',
+                'Title' => 'Title',
+                'Languages' => 'Lang'
+            ];
 
-		}
-	}
+            if (count($elementClasses) == 1
+                && $summaryFields = singleton($elementClasses[0])->summaryFields()
+            ) {
+                $columns = array_merge($columns, $summaryFields);
+            }
 
-	public function getCMSElements() {
-		return $this->owner->Elements()->sort(array('Sort'=>'ASC'));
-	}
+            $config
+                ->getComponentByType('GridFieldDataColumns')
+                ->setDisplayFields($columns)
+            ;
 
-	public function onAfterPublish() {
-		foreach($this->owner->Elements() as $element) {
-			$element->write();
-			$element->publish('Stage', 'Live');
-		}
-	}
+            $fields->addFieldToTab('Root.Elements',
+                $gridfieldItems = GridField::create(
+                    'Elements',
+                    _t('ElementsExtension.Elements', 'Elements'),
+                    $this->owner->getCMSElements(),
+                    $config
+                )
+            );
+        }
+    }
+
+    protected function _elementClassesForDropdown($elementClasses)
+    {
+        $result = [];
+        foreach ($elementClasses as $elementClass)
+        {
+            $result[$elementClass] = $elementClass;
+            if ($label = singleton($elementClass)->stat('singular_name'))
+            {
+                $result[$className] = $label;
+            }
+            return $result;
+        }
+    }
+
+    protected function _elementRealtionNames()
+    {
+        if ($elementRelations = $this->owner->config()->element_relations)
+        {
+            return array_keys($elementRelations);
+        }
+        return false;
+    }
+
+    protected function _elementClassesForRealtion($relationName)
+    {
+        if ($elementRelations = $this->owner->config()->element_relations)
+        {
+            if (isset($elementRelations[$relationName]))
+            {
+                return array_filter($elementRelations[$relationName], function($className)
+                {
+                    if (ClassInfo::exists($className) && (is_a(singleton($className), $this->_elementBaseClass)))
+                    {
+                        return $className;
+                    }
+                });
+            }
+        }
+        return [];
+    }
 }
--- code/models/ElementBase.php
+++ code/models/ElementBase.php
@@ -1,110 +1,134 @@
 <?php
-class ElementBase extends DataObject implements CMSPreviewable {
+class ElementBase extends DataObject implements CMSPreviewable
+{
+    private static $db = array(
+        'URLSegment' => 'Varchar(255)',
+        'Sort' => 'Int'
+    );
 
-	private static $db = array(
-		'URLSegment' => 'Varchar(255)',
-		'Sort' => 'Int'
-		);
+    private static $has_one = array(
+        'Page' => 'Page'
+    );
 
-	private static $has_one = array(
-		'Page' => 'Page'
-		);
+    private static $default_sort = 'Sort ASC';
 
-	private static $default_sort = 'Sort ASC';
+    private static $extensions = array(
+        'GridFieldIsPublishedExtension',
+        'Versioned("Stage","Live")'
+    );
 
-	private static $extensions = array(
-		'GridFieldIsPublishedExtension',
-		'Versioned("Stage","Live")'
-		);
+    private static $translate = 'none';
 
-	private static $translate = 'none';
+    private static $searchable_fields = array(
+        'ClassName',
+        'URLSegment'
+    );
 
-	private static $searchable_fields = array(
-		'ClassName',
-		'URLSegment'
-		);
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        if (!$this->Sort)
+        {
+            $this->Sort = ElementBase::get()
+                ->filter(array('PageID' => $this->PageID))
+                ->max('Sort') + 1;
+        }
+    }
 
-	public function onBeforeWrite(){
-		parent::onBeforeWrite();
-		if(!$this->Sort){
-			$this->Sort = ElementBase::get()
-			->filter(array('PageID' => $this->PageID))
-			->max('Sort') + 1;
-		}
-	}
+    public function addCMSFieldsHeader($fields, $page)
+    {
+        // @todo use $page->stat('elements') or $page->config()->elements to make it inheritable?
+        $recordClassesMap = ElementsExtension::parse_classes($page->stat('elements'));
+        $description = '<div class="cms-page-info"><b>'. $this->i18n_singular_name() . '</b> – ID: ' . $this->ID;
+        if (ClassInfo::exists('Fluent'))
+        {
+            $description .= ' – Locale: <img src="'. ELEMENTS_DIR . '/images/languages/'. Fluent::current_locale() .'.gif"> ' . Fluent::current_locale() . '</div>';
+        }
 
-	public function addCMSFieldsHeader($fields, $page){
-		// TODO: check fluent dependency
-		$recordClassesMap = ElementsExtension::parseClasses($page->stat('elements'));
-		$fields->addFieldToTab('Root.Main', LiteralField::create('ClassNameDescription',
-			'<div class="cms-page-info"><b>'. $this->i18n_singular_name() . '</b> – ID: ' . $this->ID . ' – Locale: <img src="'.ELEMENTS_DIR.'/images/languages/'. Fluent::current_locale() .'.gif"> ' . Fluent::current_locale() . '</div>')
-		);
-		$fields->addFieldToTab('Root.Main', DropdownField::create("ClassName", _t('ElementBase.Type', 'Type'), $recordClassesMap));
-		$fields->addFieldToTab('Root.Main', TextField::create('Title', _t('ElementBase.Title', 'Title'), null, 255));
-	}
+        $fields->addFieldsToTab('Root.Main', [
+            LiteralField::create('ClassNameDescription', $description),
+            DropdownField::create('ClassName', _t('ElementBase.Type', 'Type'), $recordClassesMap),
+            TextField::create('Title', _t('ElementBase.Title', 'Title'), null, 255)
+        ]);
+    }
 
-	public function getCMSFields() {
+    public function getCMSFields()
+    {
+        $fields = FieldList::create(TabSet::create('Root'));
 
-		$fields = FieldList::create(TabSet::create('Root'));
+        $page = $this->getHolder();
 
-		$page = $this->getHolder();
+        // if no page reference available, try to get it from the session
+        if (!$page && $currentPageID = Session::get('CMSMain.currentPage'))
+        {
+            $page = Page::get_by_id('Page', $currentPageID);
+        }
 
-	    // if no page reference available, try to get it from the session
-		if(!$page && $currentPageID = Session::get('CMSMain.currentPage')){
-			$page = Page::get_by_id('Page', $currentPageID);
-		}
+        if ($page)
+        {
+            $this->addCMSFieldsHeader($fields, $page);
+        }
+        return $fields;
+    }
 
-		if($page){
-			$this->addCMSFieldsHeader($fields, $page);
-		}
-		return $fields;
-	}
+    public function getHolder()
+    {
+        if ($this->Page()->exists()) return $this->Page();
+        return false;
+    }
 
-	public function getHolder() {
-		if($this->Page()->exists()) return $this->Page();
-		return false;
-	}
+    // remove Save & Publish to make the handling easier for the editor. Elements get published when the page gets published.
+    public function getBetterButtonsActions()
+    {
+        $fields = parent::getBetterButtonsActions();
+        $fields->removeByName('action_publish');
+        $fields->removeByName('action_unpublish');
+        return $fields;
+    }
 
-	// remove Save & Publish to make the handling easier for the editor. Elements get published when the page gets published.
-	public function getBetterButtonsActions() {
-		$fields = parent::getBetterButtonsActions();
-		$fields->removeByName('action_publish');
-		$fields->removeByName('action_unpublish');
-		return $fields;
-	}
+    public function getLanguages()
+    {
+        // TODO: check fluent dependency
+        $icons = '';
+        if (ClassInfo::exists('Fluent'))
+        {
+            $locales = $this->getFilteredLocales();
+            foreach ($locales as $key)
+            {
+                $icons .= '<img src="'.ELEMENTS_DIR.'/images/languages/'.$key.'.gif"/><br>';
+            }
+        }
+        return DBField::create_field('HTMLVarchar', $icons);
+    }
 
-	public function getLanguages(){
-		// TODO: check fluent dependency
-		$locales = $this->getFilteredLocales();
-		$icons = '';
-		foreach ($locales as $key) {
-			$icons .= '<img src="'.ELEMENTS_DIR.'/images/languages/'.$key.'.gif"/><br>';
-		}
-		return DBField::create_field('HTMLVarchar', $icons);
-	}
+    // public function Parent(){
+    //  return DataObject::get_by_id('SiteTree', $this->PageID);
+    // }
+    // public function Link($action = null) {
+    //  return Controller::join_links(Director::baseURL(), $this->RelativeLink($action));
+    // }
 
-	// public function Parent(){
-	// 	return DataObject::get_by_id('SiteTree', $this->PageID);
-	// }
-	// public function Link($action = null) {
-	// 	return Controller::join_links(Director::baseURL(), $this->RelativeLink($action));
-	// }
+    public function PreviewLink($action = null)
+    {
+        return Controller::join_links(Director::baseURL(), 'cms-preview', 'show', $this->ClassName, $this->ID);
+    }
 
-	public function PreviewLink($action = null){
-		return Controller::join_links(Director::baseURL(), 'cms-preview', 'show', $this->ClassName, $this->ID);
-	}
+    public function Link()
+    {
+        return $this->Page()->Link($this->URLSegment);
+    }
 
-	public function Link() {
-		return $this->Page()->Link($this->URLSegment);
-	}
+    public function CMSEditLink()
+    {
+        return $this->Link();
+    }
 
-	public function CMSEditLink(){
-		return $this->Link();
-	}
-
-	public function Render() {
-		$controller = Controller::curr();
-		return $controller->customise($this)->renderWith($this->ClassName);
-	}
-
+    public function Render()
+    {
+        $controller = Controller::curr();
+        return $controller
+            ->customise($this)
+            ->renderWith($this->ClassName)
+        ;
+    }
 }
